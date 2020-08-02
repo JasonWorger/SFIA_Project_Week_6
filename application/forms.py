19d0ad3e442e14bfba2609e21138b1f764b24f95
Jason Worger <jasonworger@gmail.com>
2020-08-02 20:52:27 +0100

    fixing models.py

--- application/forms.py
+++ application/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from application.models import users
+from application.models import Users
 from flask_login import current_user
 
 
