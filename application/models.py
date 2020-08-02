cd387cef22b8cbdf4f62db8214cc8ca0b69c1465
Jason Worger <jasonworger@gmail.com>
2020-08-02 21:38:13 +0100

    adding UserMixin subclass

--- application/models.py
+++ application/models.py
@@ -1,11 +1,12 @@
 from application import db, login_manager
+from flask_login import UserMixin
 
 @login_manager.user_loader
 def load_user(id):
     return Users.query.get(int(id))  
 
 # User table
-class Users(db.Model):
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
