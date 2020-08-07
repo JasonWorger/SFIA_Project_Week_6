a45224a7ecab6f0c0dcaa5509f212219db0c313e
Jason Worger <jasonworger@gmail.com>
2020-08-07 11:27:50 +0100

    adding main stock html

--- application/routes.py
+++ application/routes.py
@@ -8,84 +8,84 @@ from application.forms import RegistrationForm, LoginForm, UpdateAccountForm, Ad
 @app.route('/')
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('addProduct'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user=Users.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
-            next_page = request.args.get('next')
-            if next_page:
-                return redirect(next_page)
-            else:
-                return redirect(url_for('register'))
-    return render_template('login.html', title='Login', form=form)
+	if current_user.is_authenticated:
+		return redirect(url_for('addProduct'))
+	form = LoginForm()
+	if form.validate_on_submit():
+		user=Users.query.filter_by(email=form.email.data).first()
+		if user and bcrypt.check_password_hash(user.password, form.password.data):
+			login_user(user, remember=form.remember.data)
+			next_page = request.args.get('next')
+			if next_page:
+				return redirect(next_page)
+			else:
+				return redirect(url_for('register'))
+	return render_template('login.html', title='Login', form=form)
 
 
 #This is the register route
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('login'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hash_pw = bcrypt.generate_password_hash(form.password.data)
+	if current_user.is_authenticated:
+		return redirect(url_for('login'))
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		hash_pw = bcrypt.generate_password_hash(form.password.data)
 
-        user = Users(
-            first_name=form.first_name.data,
-            last_name=form.last_name.data,
-            email=form.email.data,
-            password=hash_pw
-        )
+		user = Users(
+			first_name=form.first_name.data,
+			last_name=form.last_name.data,
+			email=form.email.data,
+			password=hash_pw
+		)
 
-        db.session.add(user)
-        db.session.commit()
+		db.session.add(user)
+		db.session.commit()
 
-        return redirect(url_for('addProduct'))
-    return render_template('register.html', title='Register', form=form)
+		return redirect(url_for('addProduct'))
+	return render_template('register.html', title='Register', form=form)
 
 
 #This is the logout route
 @app.route("/logout")
 def logout():
-    logout_user()
-    return redirect(url_for('login'))
+	logout_user()
+	return redirect(url_for('login'))
 
 
 #Adding Product
 @app.route("/addProduct", methods=['GET', 'POST'])
 @login_required
 def addProduct():
-    form = AddProduct()
-    if form.validate_on_submit():
-        product_to_add = Product(
-            product_name = form.product_name.data,
-            product_category = form.product_category.data,
-            price = form.price.data,
-            size = form.size.data)
-        db.session.add(product_to_add)
-        db.session.commit()
-        return redirect(url_for('addStock'))
-    else:
-        print(form.errors)
-    return render_template('addProduct.html', title='Add Product', form=form)
+	form = AddProduct()
+	if form.validate_on_submit():
+		product_to_add = Product(
+			product_name = form.product_name.data,
+			product_category = form.product_category.data,
+			price = form.price.data,
+			size = form.size.data)
+		db.session.add(product_to_add)
+		db.session.commit()
+		return redirect(url_for('addStock'))
+	else:
+		print(form.errors)
+	return render_template('addProduct.html', title='Add Product', form=form)
 
 
 #Adding Stock
 @app.route("/addStock", methods=['GET', 'POST'])
 @login_required
 def addStock():
-    form = AddStock()
-    product_id = Product.query.filter_by(product_name=form.product_name.data).first()
-    if form.validate_on_submit():
-        stock_to_add = Stock(
-            product = product_id,
-            quantity = form.quantity.data)
-        db.session.add(stock_to_add)
-        db.session.commit()
-        return redirect(url_for("main_stock"))
-    return render_template('addStock.html', title='Add Stock', form=form)
+	form = AddStock()
+	product_id = Product.query.filter_by(product_name=form.product_name.data).first()
+	if form.validate_on_submit():
+		stock_to_add = Stock(
+			product = product_id,
+			quantity = form.quantity.data)
+		db.session.add(stock_to_add)
+		db.session.commit()
+		return redirect(url_for("main_stock"))
+	return render_template('addStock.html', title='Add Stock', form=form)
 
 
 #Updating Product
@@ -132,18 +132,18 @@ def updateStock(product_name):
 def deleteProduct(product_id):
 	if current_user.is_authenticated:
 		product = Product.query.filter_by(product_id = product_id).all()
-        stock = Stock.query.filter_by(product_id = product_id).all()
-        if stock:
-            for i in stock:
-                db.session.delete(i)
+		stock = Stock.query.filter_by(product_id = product_id).all()
+		if stock:
+			for i in stock:
+				db.session.delete(i)
 		for x in product:
-            db.session.delete(x) 
-        db.session.commit()
+			db.session.delete(x) 
+		db.session.commit()
 	return redirect(url_for("addProduct"))
 
 
 #Main stock list display page
 @app.route("/main_stock")
 def main_stock():
-    StockData= Stock.query.all()
-    return render_template('main_stock.html', title = "My Stock", Stock=StockData)
+	StockData= Stock.query.all()
+	return render_template('main_stock.html', title = "My Stock", Stock=StockData)
